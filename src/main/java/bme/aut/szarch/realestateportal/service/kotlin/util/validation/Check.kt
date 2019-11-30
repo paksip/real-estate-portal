93567682c0a8e0719329bfd78e78287d1795f6ed
Nagy-Gyorgy Kristof <nagygyorgykristof@gmail.com>
2019-11-30 14:43:46 +0100

    Delete contract related code from check functions

--- src/main/java/bme/aut/szarch/realestateportal/service/kotlin/util/validation/Check.kt
+++ src/main/java/bme/aut/szarch/realestateportal/service/kotlin/util/validation/Check.kt
@@ -3,10 +3,8 @@ package bme.aut.szarch.realestateportal.service.kotlin.util.validation
 import bme.aut.szarch.realestateportal.domain.User
 import org.springframework.security.access.AccessDeniedException
 import org.springframework.security.authentication.BadCredentialsException
-import kotlin.contracts.ExperimentalContracts
 
 
-@UseExperimental(ExperimentalContracts::class)
 fun User?.checkAuthentication(): User {
     if (this == null) {
         throw BadCredentialsException("User does not exist")
@@ -14,7 +12,6 @@ fun User?.checkAuthentication(): User {
     return this
 }
 
-@UseExperimental(ExperimentalContracts::class)
 inline fun <reified T : Any> T?.checkEntityNotFound(): T {
     if (this == null) {
             throw NoSuchElementException("Entity Not Found with name : ${T::class.java.name}")
@@ -22,7 +19,6 @@ inline fun <reified T : Any> T?.checkEntityNotFound(): T {
     return this
 }
 
-@UseExperimental(ExperimentalContracts::class)
 fun checkAuthorization(relatedUserId: Long, userId: Long) {
     if (relatedUserId != userId) {
         throw AccessDeniedException("Only authorized users can access the resource")
